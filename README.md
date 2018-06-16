dbe2a0ad3ca3fcb05ed2b931a75f0e4aa1d07a10
santi_iraola <santi_iraola@macbook-pro-2.lan>
2018-06-16 16:53:58 -0300

    Added readme and bold to script msgs

---
+++ README.md
@@ -0,0 +1,8 @@
+# Node server initial setup
+
+### Getting started
+
+```
+cd scripts
+bash start_dev.bash
+```
--- scripts/start_dev.bash
+++ scripts/start_dev.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo 'Building from Dockerfile'
+echo -e '\033[1mBuilding from Dockerfile\033[0m'
 bash build_docker.bash
-echo 'Starting container at port 4000'
+echo -e '\033[1mStarting container at port 4000\033[0m'
 bash start_container.bash
 bash follow_logs.bash
+echo -e '\033[1mDev server started successfully\033[0m'
