8646674a5f33f70a0541ab2ed38cbe6696444213
blam92 <iraola.santi@gmail.com>
2018-06-20 03:09:53 -0300

    Adding response to test route.

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 
-const testController = () => {
+const testController = (req, res) => {
   console.log('This is a test controller being called.');
+  res.send('Test is working');
 }
 
 module.exports = {
--- src/router.js
+++ src/router.js
@@ -3,4 +3,10 @@ const controller = require('./controllers/index.js');
 
 router.get('/test', controller.testController);
 
+router.get('/', (req, res) => {
+  res.send({
+    serverWorking: true
+  });
+});
+
 module.exports = router;
